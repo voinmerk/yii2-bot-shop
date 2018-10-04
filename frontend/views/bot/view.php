0d38b9f20c22b60b6f0241d2afa8d2bbc736c64c
Evgeniy Voyt <kremniov96@gmail.com>
2018-10-04 15:41:44 +0700

    Исправлен баг в хлебных крошках шаблона bot/view

--- frontend/views/bot/view.php
+++ frontend/views/bot/view.php
@@ -6,8 +6,12 @@ use yii\helpers\Html;
 $this->title = Yii::t('frontend', 'Catalog') . ' - ' . $bot->meta_title;
 
 $this->params['breadcrumbs'][] = [
+    'label' => Yii::t('frontend', 'Bots Catalog'),
+    'url' => Url::to(['bot/index']),
+];
+$this->params['breadcrumbs'][] = [
     'label' => $category->title,
-    'url' => ['bot/category', 'slug' => $category->slug],
+    'url' => ['bot/category', 'category' => $category->slug],
 ];
 $this->params['breadcrumbs'][] = $bot->title;
 ?>
@@ -18,7 +22,7 @@ $this->params['breadcrumbs'][] = $bot->title;
 
     <div class="row">
         <div class="col-md-12">
-            
+
         </div>
     </div>
 </div>
